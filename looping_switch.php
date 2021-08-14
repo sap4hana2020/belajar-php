ae1b2559998f35c0b9cf38dd08d756d3a5041d81
sap4hana2020 <88936120+sap4hana2020@users.noreply.github.com>
2021-08-14 21:40:12 +0700

    Latihan penggunaan switch

---
+++ looping_switch.php
@@ -0,0 +1,16 @@
+<?php
+$level = 10;
+
+switch($level) {
+case 1:
+  echo "Level anda HTML";
+  break;
+case 2:
+  echo "Level anda CSS";
+  break;
+case 3:
+  echo "Level anda Javascript";
+  break;
+default:
+  echo "Anda bukan programmer";
+}
