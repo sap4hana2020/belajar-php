7490cfa40256d2a2f60e1e8d4729b220f776c529
sap4hana2020 <88936120+sap4hana2020@users.noreply.github.com>
2021-08-15 15:02:01 +0700

    update tes2.php

---
+++ tes2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>tez</title>
+    </head>
+    <body>
+        
+        <?php
+        
+        echo "tes <br>";
+        echo "coa";
+        
+        $a=10;
+        
+        ?>
+    </body>
+</html>
+
