15afd08b4f570792b50296a106076d8ecd9cc00e
sap4hana2020 <88936120+sap4hana2020@users.noreply.github.com>
2021-08-16 19:34:51 +0700

    update perulangan-for2.php

---
+++ perulangan-for2.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+        
+        for($i=0;$i<20;$i++){
+            echo "coba perulangan <br> ";
+        }
+        
+        ?>
+        
+    </body>
+</html>
